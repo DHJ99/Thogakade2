1ec4fa30fce5f74d065f07783de75c7e523a2a1a
DHJ99 <dananjayamadushanka825@gmail.com>
2024-08-16 22:14:45 +0530

    New

--- src/main/java/controller/ViewCustomerFormController.java
+++ src/main/java/controller/ViewCustomerFormController.java
@@ -5,51 +5,64 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.Customer;
 import thogakadePOS.DBConnection;
 
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class ViewCustomerFormController {
+public class ViewCustomerFormController implements Initializable {
 
     @FXML
     public JFXButton btnReload;
 
     @FXML
-    private TableColumn<?, ?> colAddress;
+    private TableColumn<Customer, String> colAddress;
 
     @FXML
-    private TableColumn<?, ?> colContact;
+    private TableColumn<Customer, String> colContact;
 
     @FXML
-    private TableColumn<?, ?> colDob;
+    private TableColumn<Customer, String> colDob;
 
     @FXML
-    private TableColumn<?, ?> colId;
+    private TableColumn<Customer, String> colId;
 
     @FXML
-    private TableColumn<?, ?> colName;
+    private TableColumn<Customer, String> colName;
 
     @FXML
     private TableView<Customer> tblCustomers;
 
 
-    @FXML
-    void btnReload(ActionEvent ignoredEvent) {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupTableColumns();
+        loadCustomerData();
+    }
+
+    private void setupTableColumns() {
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
         colName.setCellValueFactory(new PropertyValueFactory<>("name"));
         colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
         colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
         colDob.setCellValueFactory(new PropertyValueFactory<>("dob"));
+    }
 
+    private void loadCustomerData() {
+        ObservableList<Customer> customerObservableList;
         List<Customer> customerList = DBConnection.getInstance().getConnection();
-        ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
-
-        customerObservableList.addAll(customerList);
+        customerObservableList = FXCollections.observableArrayList(customerList);
         tblCustomers.setItems(customerObservableList);
     }
 
+    @FXML
+    void btnReload(ActionEvent ignoredEvent) {
+        loadCustomerData();
+    }
 }
